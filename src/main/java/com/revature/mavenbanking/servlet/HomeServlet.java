e1da23b6f78f88b06b71ce86bbd1cd9914fc0168
kdemello1980 <k6demello28@fastmail.fm>
2020-06-11 15:45:43 -0400

    ignore target directory

---
+++ src/main/java/com/revature/mavenbanking/servlet/HomeServlet.java
@@ -0,0 +1,27 @@
+package com.revature.mavenbanking.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -7879124574949421808L;
+	
+	public void init() throws ServletException {
+		super.init();
+	}
+	
+	public void destroy(){
+		super.destroy();
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		out.println(req.getAttribute("message"));
+	}
+}

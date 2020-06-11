e8e2cb14e53fbb64d01f48010754a1dea992a998
kdemello1980 <k6demello28@fastmail.fm>
2020-06-11 15:43:32 -0400

    Add example code for sessions and cookies

--- src/main/java/com/revature/mavenbanking/servlet/LoginServlet.java
+++ src/main/java/com/revature/mavenbanking/servlet/LoginServlet.java
@@ -2,6 +2,8 @@ package com.revature.mavenbanking.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,14 +33,20 @@ public class LoginServlet extends HttpServlet {
 		PrintWriter out = res.getWriter();
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
+		RequestDispatcher rd = null;
 		
 		if (email != null && password != null){
 			UserDaoImpl udi = new UserDaoImpl();
 			User u = udi.getUserByEmail(email);
 			if (u != null && u.getPassword().equals(password)){
-				out.println("Login success");
+//				out.println("Login success");
+				rd = req.getRequestDispatcher("/home");
+				req.setAttribute("message", "Access granted! Welcome to MavenBank!");
+				rd.forward(req, res);
 			} else {
-				out.println("Login failure: Incorrect password.");
+//				out.println("Login failure: Incorrect password.");
+				rd = req.getRequestDispatcher("login.html");
+				rd.include(req, res);
 			}
 			
 		} else {
---
+++ src/main/java/com/revature/mavenbanking/servlet/TargetServlet.java
@@ -0,0 +1,39 @@
+package com.revature.mavenbanking.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class TargetServlet
+ */
+public class TargetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String username = (String) session.getAttribute("user");
+		
+		Cookie[] cookies = request.getCookies();
+
+		for (int i = 0; i < cookies.length; i++){
+			System.out.println(cookies[i].getName());
+			System.out.println(cookies[i].getValue());
+		}
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Our username is: " + username + "</h1>");
+	}
+
+}

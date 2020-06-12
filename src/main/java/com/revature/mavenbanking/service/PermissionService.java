bf1e639135acfae5e60a84b9a8de2a500ab9c26d
kdemello1980 <k6demello28@fastmail.fm>
2020-06-12 19:32:51 -0400

    Added exceptions to all service classes.

--- src/main/java/com/revature/mavenbanking/service/PermissionService.java
+++ src/main/java/com/revature/mavenbanking/service/PermissionService.java
@@ -10,27 +10,48 @@ public class PermissionService {
 
 	private PermissionDaoImpl pdi = new PermissionDaoImpl();
 	
-	public boolean addPermission(String name){
-		return pdi.addPermission(name);
+	public boolean addPermission(String name) throws Exception {
+		if (pdi.addPermission(name))
+			return true;
+		else
+			throw new Exception("Failed to add permission: " + name);
 	}
 	
-	public boolean addRolePermission(Role role, Permission permission){
-		return pdi.addRolePermission(role, permission);
+	public boolean addRolePermission(Role role, Permission permission) throws Exception {
+		if (pdi.addRolePermission(role, permission))
+			return true;
+		else
+			throw new Exception("Failed to add " + permission.getPermissionName() + " to " + role.getRole());
 	}
 	
-	public ArrayList<Permission> getAllPermissions(){
-		return pdi.getAllPermissions();
+	public ArrayList<Permission> getAllPermissions() throws Exception {
+		ArrayList<Permission> list = pdi.getAllPermissions();
+		if (list != null)
+			return list;
+		else
+			throw new Exception("Failed to retrieve all permissions.");
 	}
 	
-	public Permission getPermissionByID(int id){
-		return pdi.getPermissionByID(id);
+	public Permission getPermissionByID(int id) throws Exception {
+		Permission perm = pdi.getPermissionByID(id);
+		if (perm != null)
+			return perm;
+		else
+			throw new Exception("Failed to retrieve permission id: " + id);
 	}
 	
-	public ArrayList<Permission> getPermissionsByRoleId(int id){
-		return pdi.getPermissionsByRoleId(id);
+	public ArrayList<Permission> getPermissionsByRoleId(int id) throws Exception {
+		ArrayList<Permission> perms = pdi.getPermissionsByRoleId(id);
+		if (perms != null)
+			return perms;
+		else
+			throw new Exception("Failed to get permission for role: " + id);
 	}
 	
-	public boolean removeRolePermission(Role role, Permission permission){
-		return pdi.removeRolePermission(role, permission);
+	public boolean removeRolePermission(Role role, Permission permission) throws Exception {
+		if (pdi.removeRolePermission(role, permission))
+			return true;
+		else
+			throw new Exception("Failed to remove " + permission.getPermissionName() + " from " + role.getRole());
 	}
 }
--- src/main/java/com/revature/mavenbanking/service/RoleService.java
+++ src/main/java/com/revature/mavenbanking/service/RoleService.java
@@ -8,27 +8,48 @@ import com.revature.mavenbanking.model.Role;
 public class RoleService {
 	private RoleDaoImpl rdi = new RoleDaoImpl();
 	
-	public boolean addRole(Role r){
-		return rdi.addRole(r);
+	public boolean addRole(Role r) throws Exception {
+		if (rdi.addRole(r))
+			return true;
+		else
+			throw new Exception("Failed to add role: " + r.getRole());
 	}
 	
-	public boolean deleteRoleById(int id){
-		return rdi.deleteRoleById(id);
+	public boolean deleteRoleById(int id) throws Exception {
+		if (rdi.deleteRoleById(id))
+			return true;
+		else
+			throw new Exception("Failed to delete role id: " + id);
 	}
 	
-	public ArrayList<Role> getAllRoles(){
-		return rdi.getAllRoles();
+	public ArrayList<Role> getAllRoles() throws Exception {
+		ArrayList<Role> list = rdi.getAllRoles();
+		if (list != null)
+			return list;
+		else
+			throw new Exception("Failed to retrieve all roles.");
 	}
 	
-	public Role getRoleById(int id){
-		return rdi.getRoleById(id);
+	public Role getRoleById(int id) throws Exception {
+		Role r = rdi.getRoleById(id);
+		if (r != null)
+			return r;
+		else
+			throw new Exception("Failed to retrieve role id: " + id);
 	}
 	
-	public Role getRoleByName(String name){
-		return rdi.getRoleByName(name);
+	public Role getRoleByName(String name) throws Exception {
+		Role r = rdi.getRoleByName(name);
+		if (r != null)
+			return r;
+		else
+			throw new Exception("Failed to retrieve role " + name); 
 	}
 	
-	public boolean udpateRole(Role r){
-		return rdi.updateRole(r);
+	public boolean udpateRole(Role r) throws Exception {
+		if (rdi.updateRole(r))
+			return true;
+		else
+			throw new Exception("Failed to update role " + r.getRole());
 	}
 }
